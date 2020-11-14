68e7276d73d93bc5ec2285e251fbe5162d2c4d58
Matheus Rocha <matheus_rocha98@outlook.com>
2020-11-13 22:37:54 -0300

    refatoracao do authfilter para analisar permissoes

--- src/main/java/br/gov/sp/fatec/trampoz_api/filters/AuthFilter.java
+++ src/main/java/br/gov/sp/fatec/trampoz_api/filters/AuthFilter.java
@@ -1,7 +1,13 @@
 package br.gov.sp.fatec.trampoz_api.filters;
 
+import br.gov.sp.fatec.trampoz_api.roles.RoleEntity;
+import br.gov.sp.fatec.trampoz_api.roles.RolesEnum;
+import br.gov.sp.fatec.trampoz_api.roles.RolesService;
+import br.gov.sp.fatec.trampoz_api.shared.HttpMethodsEnum;
 import br.gov.sp.fatec.trampoz_api.users.UserEntity;
 import br.gov.sp.fatec.trampoz_api.users.UserService;
+import lombok.Builder;
+import lombok.Getter;
 import org.javatuples.Pair;
 
 import javax.servlet.*;
@@ -9,21 +15,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class AuthFilter implements Filter {
 
+    @Builder
+    @Getter
+    private static class PermissionsByHttpMethod {
+        public final Set<RolesEnum> GET;
+        public final Set<RolesEnum> POST;
+        public final Set<RolesEnum> PUT;
+        public final Set<RolesEnum> DELETE;
+    }
+
     private ServletContext context;
     private UserService userService;
+    private RolesService rolesService;
+    private final AbstractMap<String, PermissionsByHttpMethod> permissionsByUrls = new HashMap<>();
     private List<String> whiteListUrls;
+
+    private void initPermissionsByUrls() {
+        this.permissionsByUrls.put("/freelancers",
+                PermissionsByHttpMethod.builder()
+                        .GET(new HashSet<>(Arrays.asList(RolesEnum.ADMIN, RolesEnum.FREELANCER,RolesEnum.CONTRACTOR)))
+                        .POST(new HashSet<>())
+                        .build());
+        this.permissionsByUrls.put("/contractors",
+                PermissionsByHttpMethod.builder()
+                        .POST(new HashSet<>())
+                        .build());
+        this.permissionsByUrls.put("/jobs",
+                PermissionsByHttpMethod.builder()
+                        .GET(new HashSet<>(Arrays.asList(RolesEnum.ADMIN, RolesEnum.FREELANCER, RolesEnum.CONTRACTOR)))
+                        .POST(new HashSet<>())
+                        .PUT(new HashSet<>(Arrays.asList(RolesEnum.ADMIN, RolesEnum.CONTRACTOR)))
+                        .DELETE(new HashSet<>(Arrays.asList(RolesEnum.ADMIN)))
+                        .build());
+    }
+
+    private void initWhiteListUrls() {
+        this.whiteListUrls = new ArrayList<>(Arrays.asList(
+                "/freelancers",
+                "/contractors",
+                "/jobs"
+        ));
+    }
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         this.context = filterConfig.getServletContext();
         this.userService = new UserService();
-        this.whiteListUrls = new ArrayList<>(Arrays.asList(
-                "/freelancers",
-                "/contractors"
-        ));
+        this.rolesService = new RolesService();
+
+        this.initWhiteListUrls();
+        this.initPermissionsByUrls();
     }
 
     @Override
@@ -31,53 +76,105 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
 
-        Boolean isUrlInWhiteList = this.whiteListUrls.contains(req.getServletPath());
-        Boolean isPost = req.getMethod().equals("POST");
-
-        if (isPost && isUrlInWhiteList) {
+        boolean IS_URL_IN_WHITELIST_URLS = this.whiteListUrls.contains(req.getServletPath());
+        boolean IS_POST = req.getMethod().equals(HttpMethodsEnum.POST.getValue());
+        if (IS_POST && IS_URL_IN_WHITELIST_URLS) {
             chain.doFilter(req, res);
             return;
         }
 
         String authHeader = req.getHeader("Authorization");
+
         if (authHeader == null) {
             this.unauthorized(res, "No authentication token provided");
             return;
         }
 
-        Pair<String, String> userCredentials = extractUserCredentials(authHeader);
+        Pair<String, String> userCredentials = extractUserCredentialsFromAuthHeader(authHeader);
         String email = userCredentials.getValue0();
         String password = userCredentials.getValue1();
 
-        if (email.isEmpty() || password.isEmpty()) {
+        boolean HAS_SOME_CREDENTIAL_EMPTY = email.isEmpty() || password.isEmpty();
+        if (HAS_SOME_CREDENTIAL_EMPTY) {
             this.unauthorized(res, "Invalid authentication token");
             return;
         }
 
-        UserEntity user = userService.findByEmail(email);
+        UserEntity user = extractUserFromDatabase(email);
         if (user == null) {
             this.unauthorized(res, "Bad credentials");
             return;
         }
 
-        Boolean credentialsAreValid = user.checkIfCredentialsAreValid(email, password);
-
-        if (!credentialsAreValid) {
+        boolean INVALID_CREDENTIALS = !user.checkIfCredentialsAreValid(email, password);
+        if (INVALID_CREDENTIALS) {
             this.unauthorized(res, "Bad credentials");
             return;
         }
 
-        chain.doFilter(req, res);
+        String method = req.getMethod();
+        String url = req.getRequestURI();
+
+        Set<RolesEnum> acceptedRolesEnums = getAcceptedRolesEnums(url, method);
+
+        boolean METHOD_NOT_IMPLEMENTED_IN_URL = acceptedRolesEnums == null;
+        if (METHOD_NOT_IMPLEMENTED_IN_URL) {
+            this.notFound(res, "Method not found in " + url);
+            return;
+        }
+
+        boolean NO_ROLE_RESTRICTION = acceptedRolesEnums.isEmpty();
+        if (NO_ROLE_RESTRICTION) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        Set<RoleEntity> acceptedRolesEntities = extractRolesEntitiesFromEnums(acceptedRolesEnums);
+        Set<RoleEntity> userRolesEntities = user.getRoles();
+
+        for (RoleEntity acceptedRole : acceptedRolesEntities) {
+            for (RoleEntity userRole : userRolesEntities) {
+                UUID acceptedRoleId = acceptedRole.getId();
+                UUID userRoleId = userRole.getId();
+
+                boolean USER_HAS_PERMISSION = acceptedRoleId.equals(userRoleId);
+                if (USER_HAS_PERMISSION) {
+                    chain.doFilter(req, res);
+                    return;
+                }
+            }
+        }
+
+        this.forbidden(res, "Insufficient permissions");
     }
 
-    private Pair<String, String> extractUserCredentials(String authHeader) {
+    private PermissionsByHttpMethod getPermissionsFromUrl(String url) {
+        return this.permissionsByUrls.get(url);
+    }
+
+    private Set<RolesEnum> getAcceptedRolesEnums(String url, String method) {
+        PermissionsByHttpMethod permissions = getPermissionsFromUrl(url);
+        if (permissions == null) return null;
+
+        Set<RolesEnum> rolesEnums;
+
+        if (method.equals(HttpMethodsEnum.GET.getValue())) rolesEnums = permissions.getGET();
+        else if (method.equals(HttpMethodsEnum.POST.getValue())) rolesEnums = permissions.getPOST();
+        else if (method.equals(HttpMethodsEnum.PUT.getValue())) rolesEnums = permissions.getPUT();
+        else if (method.equals(HttpMethodsEnum.DELETE.getValue())) rolesEnums = permissions.getDELETE();
+        else rolesEnums = null;
+
+        return rolesEnums;
+    }
+
+    private Pair<String, String> extractUserCredentialsFromAuthHeader(String authHeader) {
         Pair<String, String> emptyCredentials = new Pair<>("", "");
 
         StringTokenizer stringTokenizer = new StringTokenizer(authHeader);
         if (!stringTokenizer.hasMoreTokens()) return emptyCredentials;
 
         String basic = stringTokenizer.nextToken();
-        if (!basic.equalsIgnoreCase("Basic")) return emptyCredentials;;
+        if (!basic.equalsIgnoreCase("Basic")) return emptyCredentials;
 
         try {
             String credentials = new String(Base64.getDecoder().decode(stringTokenizer.nextToken()));
@@ -94,9 +191,30 @@ public class AuthFilter implements Filter {
         }
     }
 
+    private UserEntity extractUserFromDatabase(String email) {
+        return userService.findByEmail(email);
+    }
+
+    private RoleEntity extractRoleFromDatabase(RolesEnum role) {
+        RoleEntity foundRole = rolesService.findByName(role);
+        return foundRole;
+    }
+
+    private Set<RoleEntity> extractRolesEntitiesFromEnums(Set<RolesEnum> rolesEnums) {
+        return rolesEnums.stream().map(this::extractRoleFromDatabase).collect(Collectors.toSet());
+    }
+
     private void unauthorized(HttpServletResponse res, String message) throws IOException {
         res.setHeader("WWW-Authenticate", "Basic realm=\"" + "PROTECTED" + "\"");
         res.sendError(HttpServletResponse.SC_UNAUTHORIZED, message);
+    }
+
+    private void forbidden(HttpServletResponse res, String message) throws IOException {
+        res.sendError(HttpServletResponse.SC_FORBIDDEN, message);
+    }
+
+    private void notFound(HttpServletResponse res, String message) throws IOException {
+        res.sendError(HttpServletResponse.SC_NOT_FOUND, message);
     }
 
     @Override
