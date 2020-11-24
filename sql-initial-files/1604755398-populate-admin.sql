3c64125d4a297f30125ae44d25815bce430085c0
Matheus Rocha <matheus_rocha98@outlook.com>
2020-11-24 20:25:10 -0300

    create a new admin

--- sql-initial-files/1604755398-populate-admin.sql
+++ sql-initial-files/1604755398-populate-admin.sql
@@ -13,3 +13,19 @@ VALUES ((SELECT rol_role.rol_id FROM rol_role WHERE rol_name = 'ADMIN'), '875d37
 RETURNING *;
 
 COMMIT;
+
+BEGIN;
+
+INSERT INTO usr_user(usr_id, usr_name, usr_email, usr_password, usr_birthday)
+VALUES ('4b509f3a-78d3-4d3c-bdbc-1569a0deabd7', 'Matheus Rocha', 'admin@email.com', 'admin123', '1998-11-09')
+RETURNING *;
+
+INSERT INTO adm_admin(adm_id)
+VALUES ('4b509f3a-78d3-4d3c-bdbc-1569a0deabd7')
+RETURNING *;
+
+INSERT INTO rpu_role_per_user (rol_id, usr_id)
+VALUES ((SELECT rol_role.rol_id FROM rol_role WHERE rol_name = 'ADMIN'), '4b509f3a-78d3-4d3c-bdbc-1569a0deabd7')
+RETURNING *;
+
+COMMIT;
